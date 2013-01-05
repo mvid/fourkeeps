51e89d78d8bf29fa122d84e86912748ee3f486cc
Mantas Vidutis <mantas.a.vidutis@gmail.com>
2013-01-05 18:09:43 -0500

    db name

--- services/mongo.py
+++ services/mongo.py
@@ -1,11 +1,13 @@
+from urlparse import urlparse
 from os import environ
 from pymongo import MongoClient
 
 mongo_uri = environ.get("MONGOHQ_URL")
 
 if mongo_uri:
+  parsed = urlparse(mongo_uri)
   connection = MongoClient(mongo_uri)
+  db = connection[parsed.path]
 else:
   connection = MongoClient()
-
-db = connection.fourkeeps
+  db = connection.fourkeeps
