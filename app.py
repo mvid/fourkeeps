d85f830134c7a1075797e9072566e846428299d1
Mantas Vidutis <mantas.a.vidutis@gmail.com>
2013-01-05 14:16:08 -0500

    factor

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
-from bottle import route, run
+import bottle
 import os
 
-@route('/')
+app = bottle.Bottle()
+
+@app.route('/')
 def index():
   return "FourKeeps. Bitch."
 
-port = os.environ.get('PORT', '3000')
-run(host='0.0.0.0', port=port)
 
+
+port = os.environ.get('PORT', '3000')
+app.run(host='0.0.0.0', port=port)
