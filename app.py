350aad7c939ecca76c9ccfc87e69fa9a8d7e21b7
Cole Brown <bigswim@gmail.com>
2013-01-05 14:14:11 -0500

    home page.

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
-import bottle
+from bottle import route, run
+import os
+
+@route('/')
+def index():
+  return "FourKeeps. Bitch."
+
+port = os.environ.get('PORT', '3000')
+run(host='0.0.0.0', port=port)
+
