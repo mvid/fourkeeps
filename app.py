37126a1b29ceea486985462c071c689c3d906ff5
Cole Brown <bigswim@gmail.com>
2013-01-05 14:18:01 -0500

    Minor app updates.

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
-from bottle import route, run
+from bottle import route, run, get
 import os
 
 @route('/')
 def index():
   return "FourKeeps. Bitch."
+
+@get('/privacy')
+def privacy():
+  return "You have a sick sense of humor."
 
 port = os.environ.get('PORT', '3000')
 run(host='0.0.0.0', port=port)
