63c1cfbc5f0663fed182c6f3559b63d0adb6000a
Mantas Vidutis <mantas.a.vidutis@gmail.com>
2013-01-05 15:04:17 -0500

    freeze

--- services/foursquare.py
+++ services/foursquare.py
@@ -1,4 +1,7 @@
+from os import environ
+
 import bottle
+from pymongo import MongoClient
 import requests
 
 app = bottle.Bottle()
@@ -6,6 +9,18 @@ app = bottle.Bottle()
 @app.get("/")
 def oauth_endpoint():
   code = bottle.request.query['code']
-  user = requests.get("https://foursquare.com/oauth2/access_token", )
+  token_request = requests.get("https://foursquare.com/oauth2/access_token",
+                      params={"client_id": environ['FOURSQUARE_CLIENT_ID'],
+                              "client_secret": environ['FOURSQUARE_CLIENT_SECRET'],
+                              "grant_type": "authorization_code",
+                              "redirect_uri": environ['FOURSQUARE_REDIRECT_URI'],
+                              "code": code})
+
+  token = token_request.json['token']
+
+  user_request = requests.get("https://api.foursquare.com/v2/users/self", params={"oauth_token": token})
+  user = user_request.json['response']['user']
+
+  connection = MongoClient(environ.get("MONGO_HOST", ""), environ.get("MONGO_PORT", 27017))
 
 application = app
