5fe0a4b2f62a27370843c453b55c5660ae1208f7
Cole Brown <bigswim@gmail.com>
2013-01-05 18:18:54 -0500

    login shit

--- services/mongo.py
+++ services/mongo.py
@@ -7,8 +7,9 @@ mongo_uri = environ.get("MONGOHQ_URL")
 if mongo_uri:
   parsed = urlparse(mongo_uri)
   connection = MongoClient(mongo_uri)
-  db = connection[parsed.path]
+  db = connection[parsed.path[1:]]
   db.authenticate(parsed.username, parsed.password)
 else:
   connection = MongoClient()
   db = connection.fourkeeps
+
